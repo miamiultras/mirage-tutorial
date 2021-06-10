289020aa08e2acc01616fdbaa073fa1fd02a8183
miamiultras <zurawinaiserow@gmail.com>
2021-06-10 22:04:53 +0000

    add dynamic handlers

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-// Welcome to the tutorial!
+import { createServer, Model } from "miragejs";
+
+export default function () {
+  createServer({
+    models: {
+      reminder: Model,
+    },
+
+    routes() {
+      this.get("/api/reminders", (schema) => {
+        return schema.reminders.all();
+      });
+
+      this.post("/api/reminders", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody);
+
+        return schema.reminders.create(attrs);
+      });
+    },
+  });
+}
